a87afe7ac6db9bba1bdc615a0573222aa90768ef
Adrià Lisa Bou <105350856+AdriaCoding@users.noreply.github.com>
2024-06-11 21:11:14 +0200

    Fixed overflow bug

--- treaps/treaps2.cpp
+++ treaps/treaps2.cpp
@@ -33,9 +33,17 @@ void print_tree(Treap t) {
     if (!t) return;
     print_tree(t->l);
     std::cout << "Key: " << t->key << ", Sum: " << t->sum << ", Size: " << t->size << "\n";
-    cout << "Left: " << (t->l ? t->l->key : -1) << ", Right: " << (t->r ? t->r->key : -1) << endl;
+    cout << "   Left: " << (t->l ? t->l->key : -1) << ", Right: " << (t->r ? t->r->key : -1) << endl;
     print_tree(t->r);
 }
+
+void print_set(Treap t) {
+    if (!t) return;
+    print_set(t->l);
+    std::cout << t->key << "(" << t->sum << ") , ";
+    print_set(t->r);
+}
+
 
 void rotate_right(Treap& t) {
     Treap temp = t->l;
@@ -58,25 +66,25 @@ void rotate_left(Treap& t) {
 bool insert(Treap& t, int key) {
     if (!t) {
         t = new Node(key);
-        return false;
-    }
-    if (key == t->key) {
         return true;
     }
-    bool isPresent;
+    if (key == t->key) {
+        return false;
+    }
+    bool inserted;
     if (key < t->key) {
-        isPresent = insert(t->l, key);
+        inserted = insert(t->l, key);
         if (t->l && t->l->priority > t->priority) {
             rotate_right(t);
         }
     } else {
-        isPresent = insert(t->r, key);
+        inserted = insert(t->r, key);
         if (t->r && t->r->priority > t->priority) {
             rotate_left(t);
         }
     }
     update(t);
-    return isPresent;
+    return inserted;
 }
 
 int sum_range(Treap t, int i, int j) {
@@ -85,7 +93,10 @@ int sum_range(Treap t, int i, int j) {
     int left_size = t->l ? t->l->size : 0;
     if (j <= left_size) return sum_range(t->l, i, j)%MOD;
     if (i > left_size + 1) return sum_range(t->r, i - left_size - 1, j - left_size - 1)%MOD;
-    return ((t->l ? sum_range(t->l, i, left_size) : 0) + t->key + (t->r ? sum_range(t->r, 1, j - left_size -1) : 0)) % MOD;
+
+    int left_range = t->l ? sum_range(t->l, i, left_size)%MOD : 0;
+    int right_range = t->r ? sum_range(t->r, max(1, i - left_size - 1), j - left_size - 1)%MOD : 0;
+    return ((left_range + t->key)%MOD + right_range) % MOD;
 }
 
 int main() {
@@ -100,14 +111,16 @@ int main() {
             //print_tree(root);
             //cout << "sum_range " << i << " " << j << ": "<< sum_range(root, i, j) << endl;            
             int z = (y + sum_range(root, i, j)) % MOD;
-            if (insert(root, z)) {
+            if (!insert(root, z)) {
                 std::cout << "R " << z << "\n";
             } else {
                 std::cout << "I " << z << "\n";
             }
         }
-        //if (m == 10) cout << sum_range(root, 3, 5) << endl;
+        //cout << " The full set is: "; print_set(root); cout << endl;
+        //print_tree(root);
         cout << "----------" << endl;
+        delete root;
     }
     return 0;
 }
